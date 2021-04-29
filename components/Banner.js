f73b382fc0ab1ccca35f4bc547f5088dc43d09ba
billyct <billy_allen@126.com>
2021-04-30 00:33:40 +0800

    chg: add z-50 to banner component

--- components/Banner.js
+++ components/Banner.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 
 const Banner = () => (
-  <div className='fixed bg-white top-0 left-0 w-full text-center text-gray-800 h-12'>
+  <div className='fixed bg-white top-0 left-0 w-full text-center text-gray-800 h-12 z-50'>
     <div className='text-sm h-full flex items-center justify-center'>
       <span className='bg-red-500 text-white p-1 rounded text-sm uppercase mr-2'>NEW</span>
       <Link href='/vscode'>
